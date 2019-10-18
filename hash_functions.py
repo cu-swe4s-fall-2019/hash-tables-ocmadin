03e53734300ca46b512aa8df8b2784662773b0ae
owenmadin <owen.madin@colorado.edu>
2019-10-17 23:10:11 -0600

    PEP8 compliance

--- hash_functions.py
+++ hash_functions.py
@@ -1,18 +1,18 @@
 
 def h_ascii(key, N):
-    
+
     if key is None:
         raise TypeError('h_ascii: must supply a key')
-    
+
     if not isinstance(key, str):
         raise TypeError('h_ascii: key must be a string')
-    
+
     if N is None:
         raise TypeError('h_ascii: must supply a integer to N')
-    
+
     if not isinstance(N, int):
         raise TypeError('h_ascii: N must be an integer')
-        
+
     s = 0
     for i in range(len(key)):
         s += ord(key[i])
@@ -20,23 +20,21 @@ def h_ascii(key, N):
 
 
 def h_rolling(key, N, p=53, m=2**64):
-    
+
     if key is None:
         raise TypeError('h_ascii: must supply a key')
-    
+
     if not isinstance(key, str):
         raise TypeError('h_ascii: key must be a string')
-    
+
     if N is None:
         raise TypeError('h_ascii: must supply a integer to N')
-    
+
     if not isinstance(N, int):
-        raise TypeError('h_ascii: N must be an integer')   
-    
-    
+        raise TypeError('h_ascii: N must be an integer')
+
     s = 0
     for i in range(len(key)):
         s += ord(key[i]) * p**i
     s = s % m
-    return s % N 
-
+    return s % N
