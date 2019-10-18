24ab96bbbea94bf308f8728192981d8c1d20529f
owenmadin <owen.madin@colorado.edu>
2019-10-17 19:07:37 -0600

    Added basic input tests for h_rolling

--- hash_functions.py
+++ hash_functions.py
@@ -20,4 +20,18 @@ def h_ascii(key, N):
 
 
 def h_rolling(key, N):
+    
+    if key is None:
+        raise TypeError('h_ascii: must supply a key')
+    
+    if not isinstance(key, str):
+        raise TypeError('h_ascii: key must be a string')
+    
+    if N is None:
+        raise TypeError('h_ascii: must supply a integer to N')
+    
+    if not isinstance(N, int):
+        raise TypeError('h_ascii: N must be an integer')   
+    
+    
     return None
--- test_hash_functions.py
+++ test_hash_functions.py
@@ -25,6 +25,21 @@ class testHASCII(unittest.TestCase):
     def test_output_random_input(self):
         for i in range(1000):
             random_hash = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(32)])
-            random_N = random.randint(0,1000)
+            random_N = random.randint(1,1000)
             self.assertLess(hf.h_ascii(random_hash,random_N),random_N)
+
+class testHRolling(unittest.TestCase):
+    def test_no_input_key(self):
+        self.assertRaises(TypeError,hf.h_rolling,None,100)
+    def test_wrong_input_type_key(self):
+        self.assertRaises(TypeError,hf.h_rolling,1,100)
+        self.assertRaises(TypeError,hf.h_rolling,[10,10,10],100)
+        self.assertRaises(TypeError,hf.h_rolling,{},100)
+        
+    def test_no_input_N(self):
+        self.assertRaises(TypeError,hf.h_rolling,'text',None)
+    def test_wrong_input_type_N(self):
+        self.assertRaises(TypeError,hf.h_rolling,'text','text')
+        self.assertRaises(TypeError,hf.h_rolling,'text',['text','text','text'])
+        self.assertRaises(TypeError,hf.h_rolling,'text',{})    
             
